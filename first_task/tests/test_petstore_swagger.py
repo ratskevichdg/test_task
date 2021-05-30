f695dcf59fdffd3a643a093511d7628789e93ae7
ratskevichdg <ratskevichdg@gmail.com>
2021-05-30 23:03:52 +0300

    add additional asserts for CRUD testing

--- first_task/tests/test_petstore_swagger.py
+++ first_task/tests/test_petstore_swagger.py
@@ -21,8 +21,11 @@ def test_create_with_list():
         data=json.dumps(INPUT_LIST),
         headers=HEADERS,
     )
+    recently_added_user = requests.get(
+        f"https://petstore.swagger.io/v2/user/{INPUT_LIST[0]['username']}"
+    )
     assert response.status_code == 200
-    assert response.json()["message"] == "ok"
+    assert recently_added_user.json()["username"] == INPUT_LIST[0]["username"]
 
 
 @mark.read
@@ -37,45 +40,53 @@ def test_get_user(usernames):
 
 @mark.update
 def test_update_user():
+    # Create new user
     requests.post(
         "https://petstore.swagger.io/v2/user",
         data=json.dumps(USER_TO_UPDATE),
         headers=HEADERS,
     )
+    # Check his lastname
     old = requests.get(
         f"https://petstore.swagger.io/v2/user/{USER_TO_UPDATE['username']}",
         headers=HEADERS,
     ).json()["lastName"]
+    # Change user info
     response = requests.put(
         f"https://petstore.swagger.io/v2/user/{USER_TO_UPDATE}",
         data=json.dumps(NEW_DATA),
         headers=HEADERS,
     )
+    # Check his lastname after editing
     new = requests.get(
         f"https://petstore.swagger.io/v2/user/{USER_TO_UPDATE['username']}",
         headers=HEADERS,
     ).json()["lastName"]
     assert response.status_code == 200
+    # Check is new lastname not equal old lastname
     assert new != old
 
 
 @mark.delete
 def test_delete_user(usernames):
+    # Create new user
     requests.post(
         "https://petstore.swagger.io/v2/user",
         data=json.dumps(USER_TO_DELETE),
         headers=HEADERS,
     )
+    # Delete user
     response = requests.delete(
         f"https://petstore.swagger.io/v2/user/{USER_TO_DELETE['username']}",
         headers={"Content-Type": "application/json"},
     )
     assert response.status_code == 200
-    response1 = requests.get(
+    # Try to get deleted user
+    response_after_deleting = requests.get(
         f"https://petstore.swagger.io/v2/user/{USER_TO_DELETE['username']}",
         headers={"Content-Type": "application/json"},
     )
-    assert response1.status_code == 404
+    assert response_after_deleting.status_code == 404
 
 
 @mark.login
